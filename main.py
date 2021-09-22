8a44054c5299d93b0a9df423ef4879f7def6e005
Arijit Ganguly <arijit.ganguly004@gmail.com>
2021-09-22 03:47:50 -0500

    Linear Regression formula implemented. Formalizing and recreating the main file

--- main.py
+++ main.py
@@ -7,42 +7,86 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.cluster import KMeans
 
-data = pd.read_csv("data/iris.data", sep=",", header=None)
-data.columns = ["Sepal_length", "Sepal_width", "Petal_length", "Petal_width", "Class"]
-print(data.describe())
+class LinearRegression:
+    
+    def __init__(self):
+        """
+         Retrieving data from the iris data set file into a dataframe
+         Preprocessing includes
+         1. Define the column names
+         2. Label Encoding the final column
+             2.1 Changing the column 'Species' type to "category"
+             2.2 Assigning numerical values and storing it in column 'Species_cat'
+             
 
-"""
-np_array = data.to_numpy()
-print(np_array)
-print(np_array.shape)
-"""
+        Returns
+        -------
+        None.
 
-#Frequency distrbiution of speciies
-iris_outcome = pd.crosstab(index = data["Class"],
-                           columns="count")
+        """
+        self.data = pd.read_csv("data/iris.data", sep=",", header=None)
+        self.data.columns = ["Sepal_length", 
+                             "Sepal_width", 
+                             "Petal_length", 
+                             "Petal_width", 
+                             "Species"]
+        self.data["Species"] = self.data["Species"].astype('category')
+        self.data["Species_cat"] = self.data["Species"].cat.codes
+    
+    def describe_data(self):
+        """
+        Generate numeric meta data on the data
+        1. Number of records for each class
+        2. Std dev, Mean, variance and other values for each of the columns
 
+        Returns
+        -------
+        None.
 
-print(iris_outcome)
+        """
+        iris_cross = pd.crosstab(index = self.data["Species"], 
+                                 columns = "count")
+        print(iris_cross)
+        print(self.data.describe())
+    
+    def visualize_data(self):
+        """
+        Creates graphs to visualize the preprocessed data
+        1. Pair plot for the data - provides the std distribution of each column
+           and column data in comparison to other columns.
 
-iris_setosa = data.loc[data["Class"] == "Iris-setosa"]
-iris_virginica = data.loc[data["Class"] == "Iris-virginica"]
-iris_versicolor = data.loc[data["Class"] == "Iris-versicolor"]
-"""
-sns.FacetGrid(data, hue="Class", size=3).map(sns.distplot, "Petal_length").add_legend()
-sns.FacetGrid(data, hue="Class", size=3).map(sns.distplot, "Petal_width").add_legend()
-sns.FacetGrid(data, hue="Class", size=3).map(sns.distplot, "Sepal_length").add_legend()
-sns.FacetGrid(data, hue="Class", size=3).map(sns.distplot, "Sepal_width").add_legend()
-plt.show()
-"""
+        Returns
+        -------
+        None.
 
-sns.set_style("whitegrid")
-sns.pairplot(data, hue="Class", size=3)
-plt.show()
+        """
+        
+        sns.set_style("whitegrid")
+        sns.pairplot(self.data, hue="Species", size=3)
+        plt.show()
+    
+    def fit(self):
+        pass
+    
+    def predict(self):
+        pass
 
+    def test_fit(self):
+        """
+        Testing fit algorithm on the entire data set
+        create A and Y
+        generate B using the formula
+        Returns
+        -------
+        None.
 
-print("Correlation between sepal length and petal length"+str(data["Sepal_length"].corr(data["Petal_length"])))
-print("Correlation between sepal length and petal width"+str(data["Sepal_length"].corr(data["Petal_width"])))
-print("Correlation between sepal width and petal width"+str(data["Sepal_width"].corr(data["Petal_width"])))
-print("Correlation between petal length and petal width"+str(data["Petal_length"].corr(data["Petal_length"])))
+        """
+        A = self.data.iloc[:, : 4].to_numpy()
+        Y = self.data.iloc[:, -1].to_numpy()
+        
+        B = np.matmul(np.matmul(np.linalg.inv(np.matmul(A.transpose(), A)),A.transpose()),Y)
+        print(B)
+        
 
-
+if __name__ == "__main__":
+    linreg = LinearRegression()
--- test.py
+++ test.py
@@ -10,8 +10,17 @@ import seaborn as sns
 from sklearn.cluster import KMeans
 
 data = pd.read_csv("data/iris.data", sep=",", header=None)
-data.columns = ["Sepal_length", "Sepal_width", "Petal_length", "Petal_width", "Class"]
+data.columns = ["Sepal_length", "Sepal_width", "Petal_length", "Petal_width", "Species"]
 print(data.describe())
+
+print("Change the column Class to category type")
+data["Species"] = data["Species"].astype('category')
+
+print("Assigning numberical values and storing in another column")
+data["Species_cat"] = data["Species"].cat.codes
+
+print(data)
+
 
 """
 np_array = data.to_numpy()
@@ -20,25 +29,25 @@ print(np_array.shape)
 """
 
 #Frequency distrbiution of speciies
-iris_outcome = pd.crosstab(index = data["Class"],
+iris_outcome = pd.crosstab(index = data["Species"],
                            columns="count")
 
 
 print(iris_outcome)
 
-iris_setosa = data.loc[data["Class"] == "Iris-setosa"]
-iris_virginica = data.loc[data["Class"] == "Iris-virginica"]
-iris_versicolor = data.loc[data["Class"] == "Iris-versicolor"]
+iris_setosa = data.loc[data["Species"] == "Iris-setosa"]
+iris_virginica = data.loc[data["Species"] == "Iris-virginica"]
+iris_versicolor = data.loc[data["Species"] == "Iris-versicolor"]
 """
-sns.FacetGrid(data, hue="Class", size=3).map(sns.distplot, "Petal_length").add_legend()
-sns.FacetGrid(data, hue="Class", size=3).map(sns.distplot, "Petal_width").add_legend()
-sns.FacetGrid(data, hue="Class", size=3).map(sns.distplot, "Sepal_length").add_legend()
-sns.FacetGrid(data, hue="Class", size=3).map(sns.distplot, "Sepal_width").add_legend()
+sns.FacetGrid(data, hue="Species", size=3).map(sns.distplot, "Petal_length").add_legend()
+sns.FacetGrid(data, hue="Species", size=3).map(sns.distplot, "Petal_width").add_legend()
+sns.FacetGrid(data, hue="Species", size=3).map(sns.distplot, "Sepal_length").add_legend()
+sns.FacetGrid(data, hue="Species", size=3).map(sns.distplot, "Sepal_width").add_legend()
 plt.show()
 """
 
 sns.set_style("whitegrid")
-sns.pairplot(data, hue="Class", size=3)
+sns.pairplot(data, hue="Species", size=3)
 plt.show()
 
 
@@ -47,5 +56,28 @@ print("Correlation between sepal length and petal width"+str(data["Sepal_length"
 print("Correlation between sepal width and petal width"+str(data["Sepal_width"].corr(data["Petal_width"])))
 print("Correlation between petal length and petal width"+str(data["Petal_length"].corr(data["Petal_length"])))
 
-
+"""
 kmeans = KMeans(n_clusters=3, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+"""
+
+print("A:")
+A = data.iloc[:, : 4].to_numpy()
+print(A)
+
+print("Y:")
+Y = data.iloc[:, -1].to_numpy()
+print(Y)
+
+print("Checking the data types:\nA:"+ str(A.dtype)+ "Y:"+str(Y.dtype))
+
+"""
+Y = Y.astype(float)
+print("Checking the data types:\nA:"+ str(A.dtype)+ "Y:"+str(Y.dtype))
+
+print(Y)
+
+"""
+
+B = np.matmul(np.matmul(np.linalg.inv(np.matmul(A.transpose(), A)),A.transpose()),Y)
+
+print(B)
