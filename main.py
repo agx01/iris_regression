8a88d9c1b37efcf9361b5824c95be8c375b3c293
Arijit Ganguly <arijit.ganguly004@gmail.com>
2021-09-25 22:15:47 -0500

    Mean beta values are generated and the accuracy scores are generated stored

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.cluster import KMeans
 from sklearn import metrics
 
 class LinearRegression:
@@ -19,7 +18,8 @@ class LinearRegression:
              2.1 Changing the column 'Species' type to "category"
              2.2 Assigning numerical values and storing it in column 'Species_cat'
              
-
+        Initializes B_list for storing Beta values from each of the bins
+        
         Returns
         -------
         None.
@@ -33,6 +33,9 @@ class LinearRegression:
                              "Species"]
         self.data["Species"] = self.data["Species"].astype('category')
         self.data["Species_cat"] = self.data["Species"].cat.codes
+        
+        #To store the Beta values in a numpy array
+        self.B_list = np.array([])
     
     def describe_data(self):
         """
@@ -110,9 +113,42 @@ class LinearRegression:
         """
         A = test_A.to_numpy()
         Y = np.matmul(A, B)
-        return Y.astype(np.float64)
+        
+        #Processing Y values to make sense
+        Y = Y.astype(np.float64)
+        Y = np.positive(Y)
+        Y = np.int32(np.rint(Y, casting='unsafe'))
+        return Y
     
-    def cross_validation(self, per_split, bins):
+    def train_test_split(self, df, per_split, species_per_bin):
+        """
+        Splits the dataframe input as per the percentage split required
+
+        Parameters
+        ----------
+        df : Pandas dataframe
+            Input dataframe.
+        per_split : float
+            Percentage split between training set and test set.
+        species_per_bin : int
+            Number of records of species per bin.
+
+        Returns
+        -------
+        train_data : Pandas dataframe
+            Training data to be used for function fit.
+        test_data : Pandas dataframe
+            Training data to be used for the function predict.
+
+        """
+        train_per_bin = int(species_per_bin * (1 - per_split))
+        test_per_bin = int(species_per_bin * per_split)
+        train_data = df.iloc[:train_per_bin, :]
+        test_data = df.iloc[-test_per_bin:, :]
+        
+        return train_data, test_data
+    
+    def cross_validation(self, per_split=0.2, bins=5):
         """
         Cross validation function
         Split the data into training and test sets to validate it across the
@@ -169,7 +205,9 @@ class LinearRegression:
                 #concatenate dataframes
                 a = i*species_per_bin
                 b = (i*species_per_bin+species_per_bin)
-                train_df, test_df = self.train_test_split(label_df.iloc[a:b, :], per_split)
+                train_df, test_df = self.train_test_split(label_df.iloc[a:b, :], 
+                                                          per_split,
+                                                          species_per_bin)
                 frames = [train_df, new_train_df]
                 new_train_df = pd.concat(frames, ignore_index=True)
                 binned_data[i]["train"] = new_train_df
@@ -178,7 +216,15 @@ class LinearRegression:
                 binned_data[i]["test"] = new_test_df
                 i += 1
         
-        B_list = []
+        #full results
+        total_results = 0
+        
+        #storing results
+        predicted_Y = {}
+        
+        #accuracyscores for each bin
+        self.accuracy_scores = {}
+        #Using the binned data we do training and testing on the binned data
         for i in range(0, bins):
             data_df = binned_data[i]["train"]
             train_A = data_df.iloc[:,:4]
@@ -187,12 +233,30 @@ class LinearRegression:
             test_A = test_data_df.iloc[:, :4]
             test_Y = test_data_df.iloc[:,-1]
             B = self.fit(train_A, train_Y)
-            Y = np.rint(np.positive(self.predict(test_A, B)), casting='unsafe')
+            Y = self.predict(test_A, B).tolist()
             test_Y = test_Y.to_list()
-            print(metrics.accuracy_score(test_Y, Y.tolist()))
-            B_list.append(B)
+            predicted_Y[i] = Y
+            
+            self.accuracy_scores[i] = metrics.accuracy_score(y_true=test_Y, y_pred=Y)
+            """
+            #Checking accuracy
+            for j in range(0, len(Y)):
+                predicted_value = Y[j]
+                actual_value = test_Y[j]
+                if predicted_value == actual_value:
+                    
+                    print(total_results)
+            """
+            self.B_list = np.append(self.B_list, B)
             i += 1
         
+        self.B_list = np.reshape(self.B_list, (bins, 4))
+        print("Beta values generated for each bin are:")
+        print(self.B_list)
+        
+        self.B_mean = np.mean(self.B_list, axis=0)
+        print("Mean Beta values generated:")
+        print(self.B_mean)
 
     def test_fit(self):
         """
@@ -214,11 +278,33 @@ class LinearRegression:
         print(B)
         
         return B
+    
+    def test_Bmean(self):
+        num_records = self.data.shape[0]
+        decision = True
+        record_no = 0
+        while decision:
+            record_no = int(input("Select record number from dataset to use for test(0 to %d):", num_records))
+            print("Record number %d has data:", record_no)
+            print(self.data[record_no])
+            continue_loop = input("Input Yes or y to select anything else:")
+            if continue_loop == 'yes' or continue_loop == 'Yes' or continue_loop == 'y' or continue_loop == 'Y':
+                decision = True
+            else:
+                decision = False
+        df = self.data[record_no].iloc[:, :4]
+        actual_value = self.data[record_no.iloc[:, -1:]]
+        B = self.B_mean
+        predicted_Y =  self.fit(df, B)
+        print("The Predicted value is:")
+        print(predicted_Y)
+        print("The Actual Value is:")
+        print(actual_value)
         
-
 if __name__ == "__main__":
     linreg = LinearRegression()
     linreg.describe_data()
     #linreg.visualize_data()
-    
-    linreg.cross_validation()
+    per_split = float(input("Please input the training and test split in decimal(20% = 0.2):"))
+    bins = int(input("Please input the number of k-folds for cross validation:"))
+    linreg.cross_validation(per_split, bins)
